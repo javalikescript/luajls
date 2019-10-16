9be48a436cce23d37d11ae9c46cdaefa7ad5a0a7
javalikescript <javalikescript@free.fr>
2019-10-16 21:25:41 +0200

    Fix pure Lua CRC32

--- tests/Crc32.lua
+++ tests/Crc32.lua
@@ -2,17 +2,20 @@ local lu = require('luaunit')
 
 local Crc32 = require('jls.util.md.Crc32')
 
+function assertHexEquals(result, expected)
+  lu.assertEquals(result, expected, string.format('expected: 0x%X, actual: 0x%X', expected, result))
+end
+
 function test_Crc32_updates()
   local crc = Crc32:new()
   crc:update('The quick brown fox')
   crc:update(' jumps over the lazy dog')
-  --print(string.format('result: 0x%x expected: 0x%x', crc:final(), 1095738169))
-  --print('crc', crc:final())
-  lu.assertEquals(crc:final(), 1095738169)
+  assertHexEquals(crc:final(), 0x414FA339)
 end
 
 function test_Crc32_digest()
-  lu.assertEquals(Crc32.digest('The quick brown fox jumps over the lazy dog'), 1095738169)
+  assertHexEquals(Crc32.digest('123456789'), 0xCBF43926)
+  assertHexEquals(Crc32.digest('The quick brown fox jumps over the lazy dog'), 0x414FA339)
 end
 
 os.exit(lu.LuaUnit.run())
