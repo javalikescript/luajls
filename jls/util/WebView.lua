667b91274aaf02a06b56525bf7c3e31e699405ba
javalikescript <javalikescript@free.fr>
2020-06-13 22:25:15 +0200

    Use thread in webview

--- jls/util/WebView.lua
+++ jls/util/WebView.lua
@@ -2,21 +2,11 @@
 -- @module jls.util.WebView
 
 local webviewLib = require('webview')
-local luvLib = require('jls.lang.loader').tryRequire('luv')
 
 local class = require('jls.lang.class')
+local Thread = require('jls.lang.Thread')
 local Promise = require('jls.lang.Promise')
 local logger = require('jls.lang.logger')
-
-function webViewInit(webView, w)
-  webView._exitPromise, webView._exitCallback = Promise.createWithCallback()
-  webView._webview = w
-end
-
-function webViewExited(webView, err)
-  webView._webview = nil
-  webView._exitCallback(err)
-end
 
 --- The WebView class.
 -- @type WebView
@@ -30,7 +20,7 @@ return class.create(function(webView)
   -- @tparam[opt] boolean resizable true if the opened window could be resized.
   -- @function WebView:new
   function webView:initialize(url, title, width, height, resizable)
-    webViewInit(self, webviewLib.new(url, title, width, height, resizable))
+    self._webview = webviewLib.new(url, title, width, height, resizable)
   end
 
   --- Processes the webview event loop.
@@ -40,7 +30,7 @@ return class.create(function(webView)
     if self._webview then
       local r = webviewLib.loop(self._webview, mode)
       if r ~= 0 then
-        webViewExited(self)
+        self._webview = nil
       end
       return r
     end
@@ -101,18 +91,6 @@ return class.create(function(webView)
     end
   end
 
-  --- Tells wether or not the webview is terminated.
-  -- @treturn boolean true if the webview is alive.
-  function webView:isAlive()
-    return self._webview ~= nil
-  end
-
-  --- Waits for the webview to terminate.
-  -- @treturn jls.lang.Promise a promise that resolves once the webview is closed.
-  function webView:wait()
-    return self._exitPromise
-  end
-
 end, function(WebView)
 
   --[[--
@@ -138,47 +116,18 @@ Opening a webview in a dedicated thread may not be supported on all platform.
 @tparam[opt] number width the width of the opened window.
 @tparam[opt] number height the height of the opened window.
 @tparam[opt] boolean resizable true if the opened window could be resized.
-@treturn jls.lang.Promise a promise that resolves once the webview is available.
+@treturn jls.lang.Promise a promise that resolves once the webview is closed.
+@treturn jls.util.WebView the created webview.
 ]]
   function WebView.open(url, title, width, height, resizable)
-    local openPromise, resolveOpen, rejectOpen = Promise.createWithCallbacks()
-    local async, thread, webview
-    async = luvLib.new_async(function(err, w)
-      logger:fine('webView:open() async received')
-      if w then
-        webview = class.makeInstance(WebView)
-        webViewInit(webview, webviewLib.fromstring(w))
-        resolveOpen(webview)
-        logger:fine('webView:open() open resolved')
-        resolveOpen = nil
-        return
-      end
-      thread:join()
-      logger:fine('webView:open() thread joined')
-      --luvLib.sleep(100)
-      webViewExited(webview, err)
-      logger:fine('webView:open() wait resolved')
-      async:close()
-    end)
-    thread = luvLib.new_thread(function(async, logLevel, url, title, width, height, resizable)
-      local logger
-      if logLevel then
-        logger = require('jls.lang.logger'):getClass():new(logLevel)
-      end
+    local webview = class.makeInstance(WebView)
+    webview._webview = webviewLib.allocate(url, title, width, height, resizable)
+    return Thread:new(function(ws)
       local webviewLib = require('webview')
-      local luvLib = require('luv')
-      local w = webviewLib.new(url, title, width, height, resizable)
-      if logger then logger:fine('webView:open() webview created') end
-      async:send(nil, webviewLib.asstring(w)) -- the webview is available
-      if logger then logger:fine('webView:open() thread looping') end
+      local w = webviewLib.fromstring(ws)
+      webviewLib.init(w)
       webviewLib.loop(w)
-      if logger then logger:fine('webView:open() thread loop ended') end
-      async:send() -- the webview is closed
-      if logger then logger:fine('webView:open() thread async sent') end
-      --luvLib.sleep(100)
-      if logger then logger:fine('webView:open() thread async closed') end
-    end, async, logger:isLoggable(logger.FINE) and logger:getLevel(), url, title, width, height, resizable)
-    return openPromise
+    end):start(webviewLib.asstring(webview._webview)):ended(), webview
   end
 
 end)
