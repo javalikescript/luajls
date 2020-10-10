fb99bf2abf7eb368f2ccd5711d0a23d098779893
javalikescript <javalikescript@free.fr>
2020-10-10 15:27:52 +0200

    Enhance EventPublisher

--- jls/util/EventPublisher.lua
+++ jls/util/EventPublisher.lua
@@ -3,6 +3,7 @@
 
 local logger = require('jls.lang.logger')
 local TableList = require('jls.util.TableList')
+local tables = require('jls.util.tables')
 
 --- The EventPublisher class.
 -- The EventPublisher provides a way subsribe and publish events.
@@ -12,8 +13,17 @@ return require('jls.lang.class').create(function(eventPublisher)
   --- Creates a new EventPublisher.
   -- @function EventPublisher:new
   -- @return a new EventPublisher
-  function eventPublisher:initialize()
+  function eventPublisher:initialize(subscriptions)
     self.eventHandlers = {}
+    if type(subscriptions) == 'table' then
+      self:subscribeEvents(subscriptions)
+    end
+  end
+
+  function eventPublisher:subscribeEvents(subscriptions)
+    for name, fn in pairs(subscriptions) do
+      self:subscribeEvent(name, fn)
+    end
   end
 
   --- Unsubscribes all the events.
@@ -22,6 +32,7 @@ return require('jls.lang.class').create(function(eventPublisher)
   end
 
   --- Subscribes to the specified event name with the specified function.
+  -- Registers an event handler/listener
   -- @tparam string name the event name
   -- @tparam function fn the function to call when the event is published
   -- @return an opaque key that could be used to unsubscribe
@@ -34,39 +45,88 @@ return require('jls.lang.class').create(function(eventPublisher)
     local eventFn = fn
     --if EventPublisher:isInstance(fn) then
     if type(fn) == 'table' and type(fn.publishEvent) == 'function' then
-      eventFn = function()
-        fn:publishEvent(name)
+      eventFn = function(...)
+        fn:publishEvent(name, ...)
       end
     end
     if type(eventFn) == 'function' then
+      if TableList.contains(handlers, eventFn) then
+        eventFn = function(...)
+          fn(...)
+        end
+      end
       table.insert(handlers, eventFn)
       return eventFn
     end
     return nil
   end
 
+  function eventPublisher:subscribeEventOnce(name, fn)
+    local key
+    key = self:subscribeEvent(name, function(...)
+      self:unsubscribeEvent(name, key)
+      fn(...)
+    end)
+    return key
+  end
+
+  function eventPublisher:eventNames()
+    local names = tables.keys(self.eventHandlers)
+    table.sort(names)
+    return names
+  end
+
+  function eventPublisher:subscriptionCount(name)
+    local handlers = self.eventHandlers[name]
+    if handlers then
+      return #handlers
+    end
+    return 0
+  end
+
   --- Unsubscribes the specified event name.
   -- @tparam string name the event name
   -- @param key the subscribed key
   function eventPublisher:unsubscribeEvent(name, key)
+    local unsubscribed = false
     local handlers = self.eventHandlers[name]
     if handlers then
-      TableList.removeFirst(handlers, key)
-      if #handlers == 0 then
+      if key then
+        unsubscribed = TableList.removeFirst(handlers, key)
+        if #handlers == 0 then
+          self.eventHandlers[name] = nil
+        end
+      else
         self.eventHandlers[name] = nil
+        unsubscribed = true
       end
     end
+    return unsubscribed
   end
 
   --- Publishes the specified event name.
+  -- Fires an event
   -- @tparam string name the event name
+  -- @treturn boolean true if the event had subscriptions, false otherwise
   function eventPublisher:publishEvent(name, ...)
     local handlers = self.eventHandlers[name]
     if handlers then
       for _, handler in ipairs(handlers) do
-        handler(...)
+        local status, err = pcall(handler, ...)
+        if not status then
+          if name == 'error' or not self:publishEvent('error', err) then
+            logger:warn('An error occurs when handling event "'..tostring(name)..'" with handler '..tostring(handler)..': '..tostring(err))
+            error(err)
+          end
+        end
+      end
+      return true
+    else
+      if logger:isLoggable(logger.FINER) then
+        logger:finer('No handler for event "'..tostring(name)..'"')
       end
     end
+    return false
   end
 
 end)
--- tests/base/EventPublisher.lua
+++ tests/base/EventPublisher.lua
@@ -2,42 +2,65 @@ local lu = require('luaunit')
 
 local EventPublisher = require("jls.util.EventPublisher")
 
-function test_subscribe_publish()
+function Test_subscribe_publish()
     local v = 0
     local ep = EventPublisher:new()
-    ep:publishEvent('test')
+    lu.assertFalse(ep:publishEvent('test'))
     lu.assertEquals(v, 0)
     ep:subscribeEvent('test', function()
       v = v + 1
     end)
     lu.assertEquals(v, 0)
-    ep:publishEvent('test')
+    lu.assertTrue(ep:publishEvent('test'))
     lu.assertEquals(v, 1)
-    ep:publishEvent('test')
+    lu.assertTrue(ep:publishEvent('test'))
     lu.assertEquals(v, 2)
 end
 
-function test_subscribe_publish_args()
+function Test_subscribe_publish_args()
   local v = 0
-  local va = nil
+  local va, vb
   local ep = EventPublisher:new()
   ep:publishEvent('test')
   lu.assertEquals(v, 0)
-  ep:subscribeEvent('test', function(a)
+  ep:subscribeEvent('test', function(a, b)
     v = v + 1
     va = a
+    vb = b
   end)
   lu.assertEquals(v, 0)
-  lu.assertEquals(va, nil)
-  ep:publishEvent('test')
+  lu.assertNil(va)
+  lu.assertNil(vb)
+  ep:publishEvent('test', 'Hi', 123)
   lu.assertEquals(v, 1)
-  lu.assertEquals(va, nil)
-  ep:publishEvent('test', 'Hi')
-  lu.assertEquals(v, 2)
   lu.assertEquals(va, 'Hi')
+  lu.assertEquals(vb, 123)
+  ep:publishEvent('test')
+  lu.assertEquals(v, 2)
+  lu.assertNil(va)
+  lu.assertNil(vb)
 end
 
-function test_subscribes_publish()
+function Test_subscribe_publish_error()
+  local capturedError
+  local ep = EventPublisher:new()
+  ep:subscribeEvent('test', function(a)
+    error('Test')
+  end)
+  lu.assertFalse(pcall(function()
+    ep:publishEvent('test')
+  end))
+  ep:subscribeEvent('error', function(err)
+    capturedError = err or true
+  end)
+  lu.assertNil(capturedError)
+  lu.assertTrue(pcall(function()
+    ep:publishEvent('test')
+  end))
+  lu.assertNotNil(capturedError)
+end
+
+function Test_subscribes_publish()
   local v = 0
   local ep = EventPublisher:new()
   ep:publishEvent('test')
@@ -55,20 +78,22 @@ function test_subscribes_publish()
   lu.assertEquals(v, 202)
 end
 
-function test_unsubscribe()
+function Test_unsubscribe()
   local v = 0
   local ep = EventPublisher:new()
+  lu.assertFalse(ep:unsubscribeEvent('test', {}))
   local eventFn = ep:subscribeEvent('test', function()
     v = v + 1
   end)
   ep:publishEvent('test')
   lu.assertEquals(v, 1)
-  ep:unsubscribeEvent('test', eventFn)
+  lu.assertTrue(ep:unsubscribeEvent('test', eventFn))
   ep:publishEvent('test')
+  lu.assertFalse(ep:unsubscribeEvent('test', eventFn))
   lu.assertEquals(v, 1)
 end
 
-function test_unsubscribeAllEvents()
+function Test_unsubscribeAllEvents()
   local v = 0
   local ep = EventPublisher:new()
   ep:subscribeEvent('test', function()
