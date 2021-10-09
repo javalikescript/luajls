96020e27184b5a72a899857d0c5df452aeedf4b6
javalikescript <javalikescript@free.fr>
2021-10-09 09:44:29 +0200

    Refactor WebView with Channel

--- jls/util/WebView.lua
+++ jls/util/WebView.lua
@@ -7,6 +7,7 @@ local class = require('jls.lang.class')
 local logger = require('jls.lang.logger')
 local event = require('jls.lang.event')
 local Thread = require('jls.lang.Thread')
+local Channel = require('jls.util.Channel')
 
 --- The WebView class.
 -- @type WebView
@@ -24,17 +25,22 @@ return class.create(function(webView)
     self._webview = webviewLib.new(url, title, width, height, resizable, debug)
   end
 
+  function webView:checkAvailable()
+    if not self._webview then
+      error('WebView not available')
+    end
+  end
+
   --- Processes the webview event loop.
   -- This function will block.
   -- @tparam[opt] string mode the loop mode, default, once or nowait.
   function webView:loop(mode)
-    if self._webview then
-      local r = webviewLib.loop(self._webview, mode)
-      if r ~= 0 then
-        self._webview = nil
-      end
-      return r
+    self:checkAvailable()
+    local r = webviewLib.loop(self._webview, mode)
+    if r ~= 0 then
+      self._webview = nil
     end
+    return r
   end
 
   --- Registers the specified function to be called from the web page.
@@ -44,9 +50,8 @@ return class.create(function(webView)
     if logger:isLoggable(logger.FINE) then
       logger:fine('webView:callback('..tostring(cb)..')')
     end
-    if self._webview then
-      webviewLib.callback(self._webview, cb)
-    end
+    self:checkAvailable()
+    webviewLib.callback(self._webview, cb)
   end
 
   --- Evaluates the specified JavaScript code in the web page.
@@ -55,9 +60,8 @@ return class.create(function(webView)
     if logger:isLoggable(logger.FINE) then
       logger:fine('webView:eval('..tostring(js)..')')
     end
-    if self._webview then
-      webviewLib.eval(self._webview, js, true)
-    end
+    self:checkAvailable()
+    webviewLib.eval(self._webview, js, true)
   end
 
   --- Sets the webview fullscreen.
@@ -66,9 +70,8 @@ return class.create(function(webView)
     if logger:isLoggable(logger.FINE) then
       logger:fine('webView:fullscreen()')
     end
-    if self._webview then
-      webviewLib.fullscreen(self._webview, fullscreen)
-    end
+    self:checkAvailable()
+    webviewLib.fullscreen(self._webview, fullscreen)
   end
 
   --- Sets the webview title.
@@ -77,9 +80,8 @@ return class.create(function(webView)
     if logger:isLoggable(logger.FINE) then
       logger:fine('webView:title('..tostring(title)..')')
     end
-    if self._webview then
-      webviewLib.title(self._webview, title)
-    end
+    self:checkAvailable()
+    webviewLib.title(self._webview, title)
   end
 
   --- Terminates the webview.
@@ -87,8 +89,10 @@ return class.create(function(webView)
     if logger:isLoggable(logger.FINE) then
       logger:fine('webView:terminate()')
     end
-    if self._webview then
-      webviewLib.terminate(self._webview, true)
+    local wv = self._webview
+    if wv then
+      self._webview = nil
+      webviewLib.terminate(wv, true)
     end
   end
 
@@ -110,141 +114,140 @@ WebView.openSync('https://www.lua.org/')
     WebView:new(url, title, width, height, resizable, debug):loop()
   end
 
-  local function readStream(stream, onMessage)
-    local buffer = ''
-    stream:readStart(function(err, data)
-      if logger:isLoggable(logger.FINEST) then
-        logger:finest('webview thread pipe read "'..tostring(err)..'", #'..tostring(data and #data))
+  function WebView._threadChannelOnlyFunction(webviewAsString, channelName, chunk, data)
+    if event:loopAlive() then
+      error('event loop is alive')
+    end
+    local fn, err = load(chunk, nil, 'b')
+    if not fn then
+      error('Unable to load chunk due to "'..tostring(err)..'"')
+    end
+    local channel = Channel:new()
+    channel:connect(channelName):catch(function(reason)
+      logger:fine('Unable to connect WebView thread due to '..tostring(reason))
+      channel = nil
+    end)
+    event:loop() -- wait for connection
+    if not channel then
+      error('Unable to connect WebView thread on "'..tostring(channelName)..'"')
+    end
+    local webview = class.makeInstance(WebView)
+    webview._channel = channel
+    webview._webview = webviewLib.fromstring(webviewAsString)
+    function webview:callback(cb)
+      webview._cb = cb
+    end
+    channel:receiveStart(function(message)
+      if webview._cb then
+        webview._cb(message)
       end
-      if err then
-      elseif data then
-        buffer = buffer..data
-        while true do
-          local bufferLength = #buffer
-          if bufferLength < 5 then
-            break
-          end
-          local messageType, remainingLength, offset = string.unpack('>BI4', buffer)
-          local messageLength = offset - 1 + remainingLength
-          if bufferLength < messageLength then
-            break
-          end
-          local remainingBuffer
-          if bufferLength == messageLength then
-            remainingBuffer = ''
-          else
-            remainingBuffer = string.sub(buffer, messageLength + 1)
-            buffer = string.sub(buffer, 1, messageLength)
-          end
-          local payload = string.sub(buffer, offset)
-          if logger:isLoggable(logger.FINEST) then
-            logger:finest('webview thread pipe type '..tostring(messageType)..', payload "'..tostring(payload)..'"')
-          end
-          onMessage(messageType, payload)
-          buffer = remainingBuffer
-        end
-        return
-      end
-      stream:close():next(function()
-        logger:finer('webview thread client pipe closed')
-      end)
+    end)
+    fn(webview, data)
+    event:loop()
+  end
+
+  -- We need to keep a reference to the thread webview to avoid GC
+  local WEBVIEW_THREAD_MAP = {}
+
+  local function registerWebViewThread(thread, webview)
+    webview._thread = thread
+    WEBVIEW_THREAD_MAP[thread] = webview
+    thread:ended():finally(function()
+      logger:fine('webview thread ended')
+      WEBVIEW_THREAD_MAP[thread] = nil
     end)
   end
 
-  local function writeStream(stream, messageType, payload)
-    if logger:isLoggable(logger.FINEST) then
-      logger:finest('webview pipe sending '..tostring(messageType)..' "'..tostring(payload)..'"')
+  function WebView.openWithThread(url, title, width, height, resizable, debug, fn, data)
+    if type(fn) ~= 'function' then
+      error('Invalid function argument')
     end
-    local writeCallback = false
-    if logger:isLoggable(logger.FINE) then
-      writeCallback = function(reason)
-        if reason then
-          logger:warn('webview pipe send error "'..tostring(reason)..'"')
-        elseif logger:isLoggable(logger.FINEST) then
-          logger:finest('webview pipe payload sent '..tostring(messageType))
+    local wv = webviewLib.allocate(url, title, width, height, resizable, debug)
+    local channelServer = Channel:new()
+    local acceptPromise = channelServer:acceptAndClose()
+    return channelServer:bind():next(function()
+      local channelName = channelServer:getName()
+      local thread = Thread:new(function(...)
+        local WV = require('jls.util.WebView')
+        WV._threadChannelOnlyFunction(...)
+      end)
+      thread:start(webviewLib.asstring(wv), channelName, string.dump(fn), data)
+      return acceptPromise
+    end):next(function(channel)
+      webviewLib.callback(wv, function(message)
+        channel:sendMessage(message, false)
+      end)
+      webviewLib.init(wv)
+      channel:onClose():next(function()
+        if wv then
+          webviewLib.terminate(wv)
         end
-      end
-    end
-    stream:write(string.pack('>Bs4', messageType, payload or ''), writeCallback)
+      end)
+      webviewLib.loop(wv) -- Will block
+      webviewLib.clean(wv)
+      wv = nil
+      channel:close(false)
+    end)
   end
 
-  local function createStreamCallback(stream)
-    return function(payload)
-      if logger:isLoggable(logger.FINER) then
-        if logger:isLoggable(logger.FINEST) then
-          logger:finest('callback(#'..tostring(payload and #payload)..'"'..tostring(payload)..'")')
-        else
-          logger:finer('callback(#'..tostring(payload and #payload)..')')
-        end
-      end
-      writeStream(stream, 1, payload)
-    end
-  end
-
-  function WebView._threadStreamFunction(webviewAsString, inThread, streamId, useTcp)
+  function WebView._threadChannelFunction(webviewAsString, channelName)
     if event:loopAlive() then
       error('event loop is alive')
     end
     local wv = webviewLib.fromstring(webviewAsString)
-    local stream, connectPromise
-    if useTcp then
-      local TcpClient = require('jls.net.TcpClient')
-      stream = TcpClient:new()
-      connectPromise = stream:connect(nil, math.tointeger(streamId))
-    else
-      local Pipe = require('jls.io.Pipe')
-      stream = Pipe:new()
-      connectPromise = stream:connect(streamId)
-    end
-    connectPromise:next(function()
-    end, function(reason)
+    local channel = Channel:new()
+    channel:connect(channelName):catch(function(reason)
       logger:fine('Unable to connect WebView thread due to '..tostring(reason))
-      stream = nil
+      channel = nil
     end)
     event:loop() -- wait for connection
-    if not stream then
-      error('Unable to connect WebView thread on "'..tostring(streamId)..'"')
+    if not channel then
+      error('Unable to connect WebView thread on "'..tostring(channelName)..'"')
     end
-    if inThread then
-      webviewLib.callback(wv, createStreamCallback(stream))
-      webviewLib.init(wv)
-      writeStream(stream, 0)
-      webviewLib.loop(wv)
-      writeStream(stream, 2)
-      webviewLib.clean(wv)
-      stream:close(false)
-      event:loop()
-      return
-    end
+    webviewLib.callback(wv, function(message)
+      channel:sendMessage(message, false)
+    end)
+    webviewLib.init(wv)
+    channel:onClose():next(function()
+      if wv then
+        webviewLib.terminate(wv)
+      end
+    end)
+    webviewLib.loop(wv)
+    webviewLib.clean(wv)
+    wv = nil
+    channel:close(false)
+    event:loop()
+  end
+
+  function WebView.openInThread(url, title, width, height, resizable, debug)
     local webview = class.makeInstance(WebView)
-    webview._webview = wv
     function webview:callback(cb)
       webview._cb = cb
     end
-    readStream(stream, function(messageType, payload)
-      if messageType == 1 then
+    local channelServer = Channel:new()
+    local acceptPromise = channelServer:acceptAndClose()
+    return channelServer:bind():next(function()
+      local channelName = channelServer:getName()
+      local thread = Thread:new(function(...)
+        local WV = require('jls.util.WebView')
+        WV._threadChannelFunction(...)
+      end)
+      local wv = webviewLib.allocate(url, title, width, height, resizable, debug)
+      thread:start(webviewLib.asstring(wv), channelName)
+      registerWebViewThread(thread, webview)
+      webview._webview = wv
+      return acceptPromise
+    end):next(function(channel)
+      webview._channel = channel
+      channel:receiveStart(function(message)
         if webview._cb then
-          webview._cb(payload)
+          webview._cb(message)
         end
-      elseif messageType == 2 then
-        stream:close(false)
-      elseif messageType == 3 then
-        local chunk, mode, value = string.unpack('>s4s1s4', payload)
-        local fn, lerr = load(chunk, nil, mode)
-        if fn then
-          fn(webview, value)
-        else
-          logger:warn('Unable to load due to "'..tostring(lerr)..'", chunk: "'..tostring(chunk)
-            ..'", mode: "'..tostring(mode)..'", value: "'..tostring(value)..'"')
-        end
-      elseif messageType == 0 then
-        -- initialized
-      else
-        logger:warn('webview pipe invalid message type '..tostring(messageType))
-      end
+      end)
+      --channel:onClose():next(function() webview:terminate() end)
+      return webview
     end)
-    --event:setInterval(function() logger:fine('webview thread looping') end, 1000)
-    event:loop()
   end
 
   function WebView._threadOpenFunction(webviewAsString, chunk, data)
@@ -264,106 +267,6 @@ WebView.openSync('https://www.lua.org/')
     webviewLib.clean(wv)
   end
 
-  -- We need to keep a reference to the thread webview to avoid GC
-  local WEBVIEW_THREAD_MAP = {}
-
-  local function threadResult(thread, webview)
-    webview._thread = thread
-    WEBVIEW_THREAD_MAP[thread] = webview
-    thread:ended():finally(function()
-      logger:fine('webview thread ended')
-      WEBVIEW_THREAD_MAP[thread] = nil
-    end)
-    return thread, webview
-  end
-
-  local USE_TCP = os.getenv('JLS_WEBVIEW_USE_TCP') ~= nil
-
-  local function openThreadStream(threadMode, url, title, width, height, resizable, debug)
-    --if event:loopAlive() then
-    --  error('event loop is alive')
-    --end
-    local thread = Thread:new(function(...)
-      local WV = require('jls.util.WebView')
-      WV._threadStreamFunction(...)
-    end)
-    local wv = webviewLib.allocate(url, title, width, height, resizable, debug)
-    local server, stream, streamId, bindPromise
-    if USE_TCP then
-      local TcpServer = require('jls.net.TcpServer')
-      server = TcpServer:new()
-      streamId = 0
-      bindPromise = server:bind(nil, 0):next(function()
-        streamId = select(2, server:getLocalName())
-      end)
-    else
-      local Pipe = require('jls.io.Pipe')
-      streamId = Pipe.normalizePipeName('WebView', true)
-      server = Pipe:new()
-      bindPromise = server:bind(streamId)
-    end
-    function server:onAccept(s)
-      stream = s
-      server:close(false)
-      event:stop() -- thread stream connected
-    end
-    bindPromise:next(function()
-      if logger:isLoggable(logger.FINE) then
-        logger:fine('webview server bound on "'..tostring(streamId)..'"')
-      end
-      thread:start(webviewLib.asstring(wv), threadMode, streamId, false)
-    end, function(reason)
-      logger:fine('webview bind error "'..tostring(reason)..'"')
-      event:stop()
-    end)
-    event:loop() -- wait for thread stream connection
-    if not stream then
-      error('Unable to connect WebView stream on "'..tostring(streamId)..'"')
-    end
-    return wv, thread, stream
-  end
-
-  function WebView.openInThread(url, title, width, height, resizable, debug, fn, data)
-    local wv, thread, stream = openThreadStream(true, url, title, width, height, resizable, debug)
-    local webview = class.makeInstance(WebView)
-    webview._webview = wv
-    function webview:callback(cb)
-      webview._cb = cb
-    end
-    if fn then
-      fn(webview, data)
-    end
-    readStream(stream, function(messageType, payload)
-      if messageType == 1 then
-        if webview._cb then
-          webview._cb(payload)
-        end
-      elseif messageType == 2 then
-        stream:close(false)
-      elseif messageType == 0 then
-        -- initialized
-      else
-        logger:warn('webview thread pipe invalid message type '..tostring(messageType))
-      end
-    end)
-    webview._stream = stream
-    return threadResult(thread, webview)
-  end
-
-  function WebView.openWithThread(url, title, width, height, resizable, debug, fn, data)
-    local wv, _, stream = openThreadStream(false, url, title, width, height, resizable, debug)
-    webviewLib.callback(wv, createStreamCallback(stream))
-    webviewLib.init(wv)
-    writeStream(stream, 0)
-    if fn then
-      writeStream(stream, 3, string.pack('>s4s1s4', string.dump(fn), 'b', data or ''))
-    end
-    webviewLib.loop(wv)
-    writeStream(stream, 2)
-    stream:close(false)
-    event:loop()
-  end
-
   --[[--
 Opens the specified URL in a new window.
 Opening a webview in a dedicated thread may not be supported on all platform.
@@ -373,11 +276,16 @@ Opening a webview in a dedicated thread may not be supported on all platform.
 @tparam[opt] number height the height of the opened window.
 @tparam[opt] boolean resizable true if the opened window could be resized.
 @tparam[opt] boolean debug true to enable devtools.
+@tparam[opt] function fn a function to be called in the webview context.
+@tparam[opt] string data the data to be passed to the function as a string.
 @treturn jls.lang.Thread the webview started thread.
 @treturn jls.util.WebView the created webview.
 ]]
   function WebView.open(url, title, width, height, resizable, debug, fn, data)
     local webview = class.makeInstance(WebView)
+    function webview:callback(cb)
+      error('The WebView callback is not avaible')
+    end
     local wv = webviewLib.allocate(url, title, width, height, resizable, debug)
     webview._webview = wv
     local thread = Thread:new(function(...)
@@ -389,7 +297,8 @@ Opening a webview in a dedicated thread may not be supported on all platform.
       chunk = string.dump(fn)
     end
     thread:start(webviewLib.asstring(webview._webview), chunk, data)
-    return threadResult(thread, webview)
+    registerWebViewThread(thread, webview)
+    return thread, webview
   end
 
   function WebView.toDataUrl(content)
