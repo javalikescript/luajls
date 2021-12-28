cd83cc85aef8fdad5ad2d42ecf204513ad307610
javalikescript <javalikescript@free.fr>
2021-12-28 21:36:48 +0100

    Test process redirect output in file

--- tests/full/process.lua
+++ tests/full/process.lua
@@ -1,7 +1,10 @@
 local lu = require('luaunit')
 
 local ProcessBuilder = require('jls.lang.ProcessBuilder')
-local Pipe = require('jls.lang.loader').tryRequire('jls.io.Pipe')
+local loader = require('jls.lang.loader')
+local Pipe = loader.tryRequire('jls.io.Pipe')
+local File = require('jls.io.File')
+local FileDescriptor = require('jls.io.FileDescriptor')
 local loop = require('jls.lang.loopWithTimeout')
 
 local logger = require('jls.lang.logger')
@@ -79,6 +82,35 @@ function Test_exitCode()
   if ph then
     lu.assertEquals(ph:isAlive(), false)
   end
+end
+
+function Test_redirect()
+  if not loader.getRequired('luv') then
+    lu.success()
+  end
+  local tmpFile = File:new('test.tmp')
+  if tmpFile:exists() then
+    tmpFile:delete()
+  end
+  local fd = FileDescriptor.openSync(tmpFile, 'w')
+  local pb = ProcessBuilder:new({LUA_PATH, '-e', 'io.write("Hello")'})
+  pb:redirectOutput(fd)
+  local exitCode
+  local ph = pb:start(function(c)
+    fd:close()
+    exitCode = c
+  end)
+  if not loop(function()
+    ph:destroy()
+    fd:close()
+    tmpFile:delete()
+  end) then
+    lu.fail('Timeout reached')
+  end
+  local output = tmpFile:readAll()
+  tmpFile:delete()
+  lu.assertEquals(exitCode, 0)
+  lu.assertEquals(output, 'Hello')
 end
 
 os.exit(lu.LuaUnit.run())
