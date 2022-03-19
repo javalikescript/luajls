213594fa229200ecd9290832fd06986fb404a6f6
javalikescript <javalikescript@free.fr>
2022-03-19 08:48:07 +0100

    Enhance protected call

--- jls/lang/protectedCall.lua
+++ jls/lang/protectedCall.lua
@@ -1,7 +1,10 @@
 local logger = require('jls.lang.logger')
-if logger:isLoggable(logger.FINE) or os.getenv('JLS_USE_XPCALL') ~= nil then
+if logger:isLoggable(logger.FINE) or os.getenv('JLS_USE_XPCALL') or _G['JLS_USE_XPCALL'] then
   return function(fn, ...)
-    return xpcall(fn, debug.traceback, ...)
+    if logger:isLoggable(logger.FINE) then
+      return xpcall(fn, debug.traceback, ...)
+    end
+    return pcall(fn, ...)
   end
 end
 return pcall
