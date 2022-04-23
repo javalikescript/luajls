f3e43ba7898b03e410fa99c10520c993f9935d4a
javalikescript <javalikescript@free.fr>
2022-04-23 17:59:53 +0200

    Add file HTTP handler test

--- tests/full/http_handlers.lua
+++ tests/full/http_handlers.lua
@@ -1,11 +1,40 @@
 local lu = require('luaunit')
 
 local loop = require('jls.lang.loopWithTimeout')
+local event = require('jls.lang.event')
+local logger = require('jls.lang.logger')
+local Promise = require('jls.lang.Promise')
+local system = require('jls.lang.system')
+local FileHttpHandler = require('jls.net.http.handler.FileHttpHandler')
 local RestHttpHandler = require('jls.net.http.handler.RestHttpHandler')
 local HttpServer = require('jls.net.http.HttpServer')
 local HttpClient = require('jls.net.http.HttpClient')
-local event = require('jls.lang.event')
-local Promise = require('jls.lang.Promise')
+local Date = require('jls.util.Date')
+
+local File = require('jls.io.File')
+
+local TEST_PATH = 'tests/full'
+local TMP_PATH = TEST_PATH..'/tmp'
+
+local function getTmpDir(forCreation)
+  local tmpDir = File:new(TMP_PATH)
+  if tmpDir:isDirectory() then
+    if forCreation then
+      if not tmpDir:deleteRecursive() then
+        error('Cannot delete tmp dir')
+      end
+    else
+      if not tmpDir:deleteAll() then
+        error('Cannot delete tmp dir content')
+      end
+    end
+  elseif not forCreation then
+    if not tmpDir:mkdir() then
+      error('Cannot create tmp dir')
+    end
+  end
+  return tmpDir
+end
 
 local function connectSendReceiveClose(httpClient, responses)
   return httpClient:connect():next(function()
@@ -17,6 +46,19 @@ local function connectSendReceiveClose(httpClient, responses)
     end
     return response
   end)
+end
+
+local function shift(responses)
+  return table.remove(responses, 1)
+end
+
+local function assertReponse(response, statusCode, body)
+  if body then
+    lu.assertEquals(response:getBody(), body)
+  end
+  if statusCode then
+    lu.assertEquals(response:getStatusCode(), statusCode)
+  end
 end
 
 function Test_rest()
@@ -79,7 +121,7 @@ function Test_rest()
       body = '<user firstname="Sally" />',
     }), responses)
   end):next(function()
-    return connectSendReceiveClose( HttpClient:new({ url = url..'/users/bar/greetings' }), responses)
+    return connectSendReceiveClose(HttpClient:new({ url = url..'/users/bar/greetings' }), responses)
   end):next(function()
     return connectSendReceiveClose(HttpClient:new({ url = url..'/users/foo' }), responses)
   end):next(function()
@@ -97,10 +139,96 @@ function Test_rest()
     lu.fail('Timeout reached')
   end
   lu.assertEquals(#responses, 6)
-  lu.assertEquals(responses[3]:getBody(), 'Hello Sally')
-  lu.assertEquals(responses[4]:getBody(), '{"firstname":"John"}')
-  lu.assertEquals(responses[5]:getBody(), '{"bar":{"firstname":"Sally"},"foo":{"firstname":"John"}}')
-  lu.assertEquals(responses[6]:getBody(), 'delay done')
+  lu.assertEquals(shift(responses):getStatusCode(), 200)
+  lu.assertEquals(shift(responses):getStatusCode(), 200)
+  lu.assertEquals(shift(responses):getBody(), 'Hello Sally')
+  lu.assertEquals(shift(responses):getBody(), '{"firstname":"John"}')
+  lu.assertEquals(shift(responses):getBody(), '{"bar":{"firstname":"Sally"},"foo":{"firstname":"John"}}')
+  lu.assertEquals(shift(responses):getBody(), 'delay done')
+end
+
+function Test_file()
+  local tmpDir = getTmpDir()
+  local responses = {}
+  local url, fileUrl, newFileUrl
+  local content = '123456789 123456789 123456789 Hello World !'
+  local httpServer = HttpServer:new()
+  httpServer:createContext('/(.*)', FileHttpHandler:new(tmpDir, 'rwl'))
+  httpServer:bind('::', 0):next(function()
+    local port = select(2, httpServer:getAddress())
+    url = 'http://127.0.0.1:'..tostring(port)
+    fileUrl = url..'/file.txt'
+    newFileUrl = url..'/file-new.txt'
+    --logger:setLevel(logger.FINE)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl, method = 'PUT', body = content }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({
+      url = fileUrl,
+      headers = {
+        ['If-Modified-Since'] = Date:new(system.currentTimeMillis() + 60000):toRFC822String(true),
+      },
+      method = 'GET'
+    }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({
+      url = fileUrl,
+      headers = {
+        ['If-Modified-Since'] = Date:new(system.currentTimeMillis() - 60000):toRFC822String(true),
+      },
+      method = 'GET'
+    }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl, headers = { Range = 'bytes=0-' }, method = 'GET' }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl, headers = { Range = 'bytes=0-9' }, method = 'GET' }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl, headers = { Range = 'bytes=4-5' }, method = 'GET' }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl, headers = { Range = 'bytes=20-' }, method = 'GET' }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl, headers = { destination = newFileUrl }, method = 'MOVE' }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = fileUrl }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = newFileUrl }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = newFileUrl, method = 'HEAD' }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = newFileUrl, method = 'DELETE' }), responses)
+  end):next(function()
+    return connectSendReceiveClose(HttpClient:new({ url = newFileUrl }), responses)
+  end):next(function()
+    httpServer:close()
+  end):catch(function(reason)
+    print('Unexpected error', reason)
+  end)
+  if not loop(function()
+    httpServer:close()
+  end) then
+    lu.fail('Timeout reached')
+  end
+  lu.assertEquals(#responses, 15)
+  assertReponse(shift(responses), 404)
+  assertReponse(shift(responses), 200) -- PUT
+  assertReponse(shift(responses), 200, content)
+  assertReponse(shift(responses), 304) -- If-Modified-Since +1mn
+  assertReponse(shift(responses), 200, content) -- If-Modified-Since -1mn
+  assertReponse(shift(responses), 206, content) -- Range 0-
+  assertReponse(shift(responses), 206, '123456789 ') -- Range 0-9
+  assertReponse(shift(responses), 206, '56') -- Range 4-5
+  assertReponse(shift(responses), 206, '123456789 Hello World !') -- Range 20-
+  assertReponse(shift(responses), 201) -- MOVE
+  assertReponse(shift(responses), 404)
+  assertReponse(shift(responses), 200, content)
+  assertReponse(shift(responses), 200, '') -- HEAD
+  assertReponse(shift(responses), 200) -- DELETE
+  assertReponse(shift(responses), 404)
+  tmpDir:deleteRecursive()
 end
 
 os.exit(lu.LuaUnit.run())
